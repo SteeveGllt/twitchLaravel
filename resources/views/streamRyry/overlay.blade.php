53a558c88a37957d1dae1105769275072a22de12
SteeveG <steeveguillot51@gmail.com>
2022-01-12 13:03:43 +0100

    View Overlay Ryry Rounded.

--- resources/views/streamRyry/overlay.blade.php
+++ resources/views/streamRyry/overlay.blade.php
@@ -5,9 +5,12 @@
     <title>Title</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body style="background-image: url('images/giphy1.gif'); background-size: cover">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Fugaz+One&family=Monoton&family=Roboto:wght@900&display=swap');
-    </style>
+<body>
+<div style="background-image: url('images/giphy1.gif'); background-size: cover" class="rounded-2xl border-2 border-violet-900 border-opacity-40 shadow-xl h-screen">
+</div>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Fugaz+One&family=Monoton&family=Roboto:wght@900&display=swap');
+</style>
 </body>
 </html>
